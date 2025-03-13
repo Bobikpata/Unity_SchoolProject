d87eef3a2fe0528e4ece8142d01a1dba743e9c89
Bobikpata <kolarikpata@gmail.com>
2025-03-13 16:30:27 +0100

    Added Pause Menu

--- Assets/Scripts/UI/UI Manager.cs
+++ Assets/Scripts/UI/UI Manager.cs
@@ -6,10 +6,28 @@ public class UIManager : MonoBehaviour
     [SerializeField] public Health playerHealth;
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private AudioClip gameOverSound;
+    [SerializeField] private GameObject pauseScreen;
 
     private void Awake()
     {
+        Time.timeScale = 1;
         gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseScreen.activeInHierarchy)
+            {
+                Pause(false);
+            }
+            else
+            {
+                Pause(true);
+            }
+        }
     }
 
     public void GameOver() //Activates Game Over screen
@@ -19,6 +37,29 @@ public class UIManager : MonoBehaviour
             gameOverScreen.SetActive(true);
             SoundManager.instance.PlaySound(gameOverSound);
         }
+    }
+
+    public void Pause(bool status)
+    {
+        pauseScreen.SetActive(status);
+
+        if (status)
+        {
+            GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = false;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Continue()
+    {
+        GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
     }
 
     //Game Over functions
